b8dbde9bba9afddc4f18d0a6a39cd286956292cd
raistlinqjx <raistlinqjx@gmail.com>
2017-12-31 14:57:57 +0800

    Creating Shots
    Add background, moving Player,Creating Shots

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
+
+	void Start(){
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update(){
+//		Input.GetButton ();
+//		Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+		if (Input.GetButton("Fire1") && Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+//			newProjectile = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+
+			// create code here that animates the newProjectile
+//
+//			nextFire = nextFire - myTime;
+//			myTime = 0.0F;
+		}
+	}
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = movement * speed;
+
+		rb.position = new Vector3 (
+			Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+			0.0f, 
+			Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+		);
+
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
+}
