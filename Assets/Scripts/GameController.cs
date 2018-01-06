b7d92602ae57e7980a43d1443cb471f946b373bf
raistlinqjx <raistlinqjx@gmail.com>
2018-01-06 22:45:02 +0800

    SpaceShooter Tutorial 2
    Hazard spawn, game controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	public GameObject hazard;
+	public Vector3 spawnValues;
+
+	void Start() {
+		SpawnWaves ();
+	}
+	void SpawnWaves(){
+		Vector3 spawnPos = new Vector3 (Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+		Quaternion spawnRot = Quaternion.identity;
+		Instantiate (hazard, spawnPos, spawnRot);
+	}
+}
